3d3b5860866755bb784e50cb8952f97551b8ccb9
wayne <waaa@gmail.com>
2022-08-26 17:29:32 +0800

    add some file

---
+++ app.js
@@ -0,0 +1,11 @@
+function convert(oc){
+    return oc *1.8 + 32;
+}
+
+let of = convert(0);
+console.log(of);
+<div class="copyright-box txt-light"></div>
+<div class="copyright-list p-tb16">
+網路家庭版權所有、轉載必究 ‧Copyright  PChome Online<p class="p-t8">PChome Online and PChome are trademarks of PChome Online Inc.</p>
+</div>         
+       
